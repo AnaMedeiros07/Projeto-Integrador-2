df818eb1e5f592fce660e2e942630450edc57651
Ines_Costa <82233326+InesCosta202@users.noreply.github.com>
2022-03-25 12:01:42 +0000

    Update PWM.c

--- CP3/PWM.c
+++ CP3/PWM.c
@@ -6,20 +6,21 @@
  */
 
 #include "PWM.h"
-
+int duty_cycle;
 
 void Init_PWM_Control (){
-	HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
 }
 
 void DeInit_PWM_Control (){
-	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_2);
+	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_1);
+	duty_cycle = 0;
+	Change_Duty();
 }
 
 _Bool Normalized_Tension(uint8_t *buffer1){
-	_Bool return_flag;
+	_Bool return_flag, Validation;
 	int row_number = 0;
-	int duty_cycle = 0;
 	char string_array[6][6];
 
 	clean_string_array(string_array);
@@ -31,22 +32,43 @@ _Bool Normalized_Tension(uint8_t *buffer1){
 	}
 
 	duty_cycle = atoi(string_array[row_number], NULL, 10);
+	Validation = Change_Duty();
+	if(Validation == 0){
+		Write_Tx_Buffer("Valores fora dos limites!!", 0);
+		return_flag = invalid;
+	}
+
+	return return_flag;
+}
+
+void Increment_Duty(){
+	duty_cycle +=5;
+	Change_Duty();
+}
+
+void Decrement_Duty(){
+	duty_cycle -=5;
+	Change_Duty();
+}
+
+_Bool Change_Duty(){
+	_Bool return_flag;
+
 	if((duty_cycle >= -100) && (duty_cycle < 0)){
 		Direction = Anti_Clock;
-		duty_cycle*=-1;
-		float duty = (float)duty_cycle/100;
-		htim4.Instance->CCR2 = duty*(htim4.Instance->ARR);
+		float duty = (float)(duty_cycle*(-1))/100;
+		htim4.Instance->CCR1 = duty*(htim4.Instance->ARR);
 		return_flag = Valid;
 	}
 	else if((duty_cycle >= 0) && (duty_cycle <= 100)){
 		Direction = Clock;
 		float duty = (float)duty_cycle/100;
-		htim4.Instance->CCR2 = duty*(htim4.Instance->ARR);
+		htim4.Instance->CCR1 = duty*(htim4.Instance->ARR);
 		return_flag = Valid;
 	}
-	else
-		return_flag = invalid;
+	else{
+		return_flag = 0;
+	}
 
 	return return_flag;
-
 }
